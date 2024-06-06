0c53d2721ae2e2824f443ca30805c56e3e3a099e
Jay <stargurl16@live.com>
2024-06-06 18:26:46 -0400

    Update List.js

--- comonents/List.js
+++ comonents/List.js
@@ -7,87 +7,88 @@ const ITEM_WIDTH = 40;
 const SPACING = 15;
 
 const PickerList = ({data, scrollY, type, pickerTextStyle}) => {
-  const renderItem = useCallback(
-      ({item, index}) => {
-          const inputRange = [
-              (index - 2) * ITEM_HEIGHT,
-              (index - 1) * ITEM_HEIGHT,
-              index * ITEM_HEIGHT,
-              (index + 1) * ITEM_HEIGHT,
-              (index + 2) * ITEM_HEIGHT,
-          ];
-          const scaleInterpolator = scrollY.interpolate({
-              inputRange,
-              outputRange:
+    
+    const renderItem=useCallback(
+        ({item,index})=>{
+            const inputRange = [
+                (index - 2) * ITEM_HEIGHT,
+                (index - 1) * ITEM_HEIGHT,
+                index * ITEM_HEIGHT,
+                (index + 1) * ITEM_HEIGHT,
+                (index + 2) * ITEM_HEIGHT,
+              ];
+              const scaleInterpolator = scrollY.interpolate({
+                inputRange,
+                outputRange:
                   type === 'clock'
-                      ? [0.8, 0.9, 1.4, 0.9, 0.8]
-                      : [0.8, 1.1, 1.7, 1.1, 0.8],
-          });
-
-          const opacityInterpolator = scrollY.interpolate({
-              inputRange,
-              outputRange: [0.3, 0.6, 1, 0.6, 0.3],
-          });
-          return (
-              <ListItem
+                    ? [0.8, 0.9, 1.4, 0.9, 0.8]
+                    : [0.8, 1.1, 1.7, 1.1, 0.8],
+              });
+      
+              const opacityInterpolator = scrollY.interpolate({
+                inputRange,
+                outputRange: [0.3, 0.6, 1, 0.6, 0.3],
+              });
+              return (
+                <ListItem
                   item={item}
                   scaleInterpolator={scaleInterpolator}
                   opacityInterpolator={opacityInterpolator}
                   pickerTextStyle={pickerTextStyle}
-              />
-          );
-      },
-      [scrollY, type, pickerTextStyle]
-  );
+                />
+              );
+        }
+        ,[]);
 
-  const keyExtractor = (item) => String(item);
-
+    const keyExtractor=(item)=>{
+        return String(item);
+    }
   return (
-      <Animated.FlatList
-          initialNumToRender={5}
-          maxToRenderPerBatch={6}
-          windowSize={6}
-          getItemLayout={(_, index) => ({
-              length: ITEM_HEIGHT,
-              offset: ITEM_HEIGHT * index,
-              index,
-          })}
-          onScroll={Animated.event(
-              [
-                  {
-                      nativeEvent: {
-                          contentOffset: {
-                              y: scrollY,
-                          },
-                      },
-                  },
-              ],
-              {useNativeDriver: true}
-          )}
-          scrollEventThrottle={16}
-          data={data}
-          keyExtractor={keyExtractor}
-          style={{
-              height: 5 * ITEM_HEIGHT,
-              flexGrow: 0,
-              width: ITEM_WIDTH + SPACING * 2,
-          }}
-          contentContainerStyle={{
-              paddingTop: 2 * ITEM_HEIGHT,
-              paddingBottom: 2 * ITEM_HEIGHT,
-              justifyContent: 'center',
-              alignItems: 'center',
-          }}
-          showsVerticalScrollIndicator={false}
-          snapToInterval={ITEM_HEIGHT}
-          decelerationRate={0}
-          snapToAlignment="center"
-          renderItem={renderItem}
-      />
+    <Animated.FlatList
+      initialNumToRender={5}
+      maxToRenderPerBatch={6}
+      windowSize={6}
+      getItemLayout={(_, index) => {
+        return {
+          length: ITEM_HEIGHT,
+          offset: ITEM_HEIGHT * index,
+          index,
+        };
+      }}
+      onScroll={Animated.event(
+        [
+          {
+            nativeEvent: {
+              contentOffset: {
+                y: scrollY,
+              },
+            },
+          },
+        ],
+        {useNativeDriver: true},
+      )}
+      scrollEventThrottle={16}
+      data={data}
+      keyExtractor={keyExtractor}
+      style={{
+        height: 5 * ITEM_HEIGHT,
+        flexGrow: 0,
+        width: ITEM_WIDTH + 2 * SPACING,
+        
+      }}
+      contentContainerStyle={{
+        paddingTop: 2 * ITEM_HEIGHT,
+        paddingBottom: 2 * ITEM_HEIGHT,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      showsVerticalScrollIndicator={false}
+      snapToInterval={ITEM_HEIGHT}
+      decelerationRate={0}
+      snapToAlignment="center"
+      renderItem={renderItem}
+    />
   );
 };
-
-//export default React.memo(PickerList);
-
 
 export default List=React.memo(PickerList);
